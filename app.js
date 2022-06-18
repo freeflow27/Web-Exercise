9c41dc0e951721a80fceb14174bd80f8d47c3f0a
Alvaro Davison <adavison813@gmail.com>
2022-06-18 10:51:45 -0400

    web exercises

---
+++ app.js
@@ -0,0 +1,20 @@
+What is HTTP?
+    Hypertext Transfer Protocal. basically getting and retreiving datta from a server.
+What is a URL?
+    Uniform Resource localStorage. THis is the hard address of the internet. Where you look specifically.
+What is DNS?
+    Domain Name System. THis is what converts our human versions to IP versions.
+What is a query string?
+    This is what you pass key-value pairs into for a more specific look into the webpage.
+What are two HTTP verbs and how are they different?
+    get, and post. get just takes information from the server, and posts allows you to upload/update information to the site itself.
+What is an HTTP request?
+    THis is the act of requesting information for ma websites server
+What is an HTTP response?
+    This is the followup to request. this is when the server responds and sends the information back to the requester.
+What is an HTTP header? Give a couple examples of request and response headers you have seen.
+        This is something that explains more specific informatiuon about the requested information.
+        response - Date, content type, server, last modified
+        request - method, accept-language, origin, refer, 
+What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+    It requests information from the srever, then checks to see if ithere are any errors, then sends back either the information or error code. THen the information build on the code and DOM. This adjusts to your browser/type of viewer. 
